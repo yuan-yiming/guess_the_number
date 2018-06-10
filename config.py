1bd6d3eac092d59d29402a38aacdc310c781b7be
Da_ming <850319041@qq.com>
2018-06-11 00:08:33 +0800

    ver 1.0

--- config.py
+++ config.py
@@ -12,7 +12,9 @@ HOST = '127.0.0.1'
 PORT = '3306'
 DATABASE = 'guess_the_number'
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOST,PORT,DATABASE)
+
+SQLALCHEMY_DATABASE_URI = DATABASE_URL
+#SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOST,PORT,DATABASE)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
